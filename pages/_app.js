4bf0005145350075cd2cd9f9844e8ae4ed061283
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2023-12-10 11:51:44 +0100

    Move vercel analytics to app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import { DataProvider } from '@/Context/DataProvider'
 import '../styles/index.scss';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return <DataProvider>
     <Component {...pageProps} />
+    <SpeedInsights />
+    <Analytics />
   </DataProvider>
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   return (
@@ -9,8 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <SpeedInsights />
-        <Analytics />
+
       </body>
     </Html>
   )
