5b958460e1a8bfecc46e4e7c80a738c476ca08cb
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-01-28 08:20:18 +0100

    Add keyword in h1 + keyword in alt

--- Components/Hero.js
+++ Components/Hero.js
@@ -13,13 +13,13 @@ const Hero = () => {
     return (
         <main id="home" className={classes.heroContainer}>
             <div className={classes.textWrapper}>
-                <h1>Développeur web</h1>
+                <h1>Développeur web freelance</h1>
                 <div className={classes.valueProposition}>
                     <p>J'aide les entreprises à obtenir plus de clients en leur développant un site internet sur mesure.</p>
                 </div>
                 <Link href="/book-a-call" className='primary-call-to-action'>Réserver un appel</Link>
             </div>
-            {(isMobileResolution || isTabletResolution) && <Image src={portfolioBgMobile} alt='Thomas André-Lubin en train de développer sur son ordinateur' />}
+            {(isMobileResolution || isTabletResolution) && <Image src={portfolioBgMobile} alt='Développeur web freelance en train de développer sur son ordinateur' />}
         </main>
     );
 };
