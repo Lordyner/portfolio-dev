940d7601fe7d1ec0dbe1f6bf6aa0b00c34f40adc
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-02-10 10:06:14 +0100

    Add new project

--- Components/Project.js
+++ Components/Project.js
@@ -5,12 +5,28 @@ import puissance4Img from '@/public/images/puissance4-screenshot.png'
 import Image from 'next/image';
 import classes from './Project.module.css';
 import frontendmentorLogo from '../public/images/frontend_mentor_logo.png';
+import payAPIHomepage from '../public/images/payAPI_homepage.png';
 
 const Project = () => {
     return (
         <section id='projects-section' className={classes.projectSection}>
             <h2>Derniers projets</h2>
             <div className={classes.container}>
+                <div className={classes.card}>
+                    <Image src={payAPIHomepage} alt="page d'accueil de payAPI" className={classes.mainImg} />
+                    <div className='d-flex gap-05 align-items-center flex-grow-2'>
+                        <Image src={frontendmentorLogo} className={classes.logo} alt="logo de l'entreprise frontendmentor.io" />
+                        <div className={classes.text}>
+                            <p className={classes.title}>Landing page PayAPI</p>
+                            <p className={classes.description}>Frontend mentor</p>
+                        </div>
+                    </div>
+                    <div className='d-flex w-100'>
+                        <a className={classes.button} href="https://pay-api-seven.vercel.app/" target="blank">
+                            Visitez le site
+                        </a>
+                    </div>
+                </div>
                 <div className={classes.card}>
                     <Image src={newsHomepageImg} alt="page d'accueil du site d'information" className={classes.mainImg} />
                     <div className='d-flex gap-05 align-items-center flex-grow-2'>
