f19fe7a9df9df92e8c0a505e697117b5acdb2a7e
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-02-17 14:04:57 +0100

    Add myTeam project

--- Components/Project.js
+++ Components/Project.js
@@ -6,12 +6,27 @@ import Image from 'next/image';
 import classes from './Project.module.css';
 import frontendmentorLogo from '../public/images/frontend_mentor_logo.png';
 import payAPIHomepage from '../public/images/payAPI_homepage.png';
-
+import myTeamLandingPage from '@/public/images/myteam_firstPage.png';
 const Project = () => {
     return (
         <section id='projects-section' className={classes.projectSection}>
             <h2>Derniers projets</h2>
             <div className={classes.container}>
+                <div className={classes.card}>
+                    <Image src={myTeamLandingPage} alt="page d'accueil de myTeam" className={classes.mainImg} />
+                    <div className='d-flex gap-05 align-items-center flex-grow-2'>
+                        <Image src={frontendmentorLogo} className={classes.logo} alt="logo de l'entreprise frontendmentor.io" />
+                        <div className={classes.text}>
+                            <p className={classes.title}>Landing page myTeam</p>
+                            <p className={classes.description}>Frontend mentor</p>
+                        </div>
+                    </div>
+                    <div className='d-flex w-100'>
+                        <a className={classes.button} href="https://my-team-eosin.vercel.app/" target="blank">
+                            Visitez le site
+                        </a>
+                    </div>
+                </div>
                 <div className={classes.card}>
                     <Image src={payAPIHomepage} alt="page d'accueil de payAPI" className={classes.mainImg} />
                     <div className='d-flex gap-05 align-items-center flex-grow-2'>
