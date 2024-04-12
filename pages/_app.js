ae293308bda54866b5d4482c8144ac2272c583fe
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-04-12 17:09:09 +0200

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,23 @@ import '../styles/index.css';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
 import { GlobalContextProvider } from '@/Store/GlobalContext';
-
+import { useRouter } from 'next/router';
+import { gtag } from '../gtag';
+import { useEffect } from 'react';
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <GlobalContextProvider>
     <Component {...pageProps} />
     <SpeedInsights />
