8dd7308223afbf4ae70fd87322afbb9865ef0e30
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-04-12 17:26:24 +0200

    Add ga for real

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,25 @@ import '../styles/index.css';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
 import { GlobalContextProvider } from '@/Store/GlobalContext';
-import { useRouter } from 'next/router';
-import { gtag } from '../gtag';
-import { useEffect } from 'react';
+import Script from 'next/script';
+
 export default function App({ Component, pageProps }) {
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+  const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_ID
 
   return <GlobalContextProvider>
     <Component {...pageProps} />
     <SpeedInsights />
     <Analytics />
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} strategy='afterInteractive' />
+    <Script id="google-analytics" strategy='afterInteractive'>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}');
+      `}
+    </Script>
+
   </GlobalContextProvider>
 }
