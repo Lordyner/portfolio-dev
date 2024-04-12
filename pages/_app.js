a8608e67f38eb845b42c34ba9223f70811773f5e
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-04-12 17:45:16 +0200

    Use GA Id directly

--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,18 @@ import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
 
-  const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_ID
   console.log(GA_TRACKING_ID)
   return <GlobalContextProvider>
     <Component {...pageProps} />
     <SpeedInsights />
     <Analytics />
-    <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} strategy='afterInteractive' />
+    <Script src='https://www.googletagmanager.com/gtag/js?id=G-S70VDK1DHC' strategy='afterInteractive' />
     <Script id="google-analytics" strategy='afterInteractive'>
       {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '${GA_TRACKING_ID}');
+        gtag('config', 'G-S70VDK1DHC');
       `}
     </Script>
 
