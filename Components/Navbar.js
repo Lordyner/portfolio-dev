907a87310f0bee06493d11a203406934929e0677
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-05-10 16:49:41 +0200

    Rewrite mobile menu animation

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -7,7 +7,7 @@ import logo from '@/public/images/global/logo.png';
 import tiktokLogo from '@/public/images/global/logo_tiktok.png';
 import instaLogo from '@/public/images/global/logo_instagram.png';
 import linkedinLogo from '@/public/images/global/logo_linkedin.png';
-
+import { animations, delay, motion, spring } from "framer-motion";
 const Navbar = () => {
 
     const burger = useRef();
@@ -15,21 +15,65 @@ const Navbar = () => {
     const { isMobileResolution } = useContext(GlobalContext);
     const { isMenuOpen, setIsMenuOpen } = useContext(GlobalContext);
     const { toggleMenu } = useContext(GlobalContext);
-
-    const variants = {
-        open: {
-            y: 0,
-            opacity: 1,
+    const sideBar = {
+        closed: {
+            width: 0,
+            padding: 0,
             transition: {
-                y: { stiffness: 1000, velocity: -100 }
-            }
+                // delay: 0.25,
+                duration: 0.55,
+            },
+        },
+        open: {
+            width: "55%",
+            padding: "6rem 3rem",
+            transition: {
+                // delay: 0.25,
+                duration: 0.55,
+
+            },
+        },
+    }
+
+    const sideVariants = {
+        closed: {
+            transition: {
+                // staggerChildren: 0.2,
+                // staggerDirection: 1,
+            },
+        },
+        open: {
+            transition: {
+                staggerChildren: 0.2,
+                staggerDirection: 1,
+            },
+        },
+    };
+    const sideVariantsSocials = {
+        closed: {
+            transition: {
+                // staggerChildren: 0.2,
+                // staggerDirection: 1,
+            },
+        },
+        open: {
+            transition: {
+                delayChildren: 0.25,
+                staggerChildren: 0.2,
+                staggerDirection: 1,
+            },
+        },
+    };
+
+    const variantsItem = {
+
+        open: {
+            y: 50,
+            opacity: 1,
+
         },
         closed: {
-            y: 50,
             opacity: 0,
-            transition: {
-                y: { stiffness: 1000 }
-            }
         }
     };
 
@@ -60,36 +104,55 @@ const Navbar = () => {
                     </div>
 
                     {/* Mobile menu */}
-                    <div className={`${classes.mobileNav} ${isMenuOpen ? classes.active : ""}`}>
-                        <div className={classes.mobileLinks}>
-
-                            <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
-                                toggleMenu();
-                                burger.current.classList.toggle(classes.isActive);
-                            }}>Accueil</Link>
-                            <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
-                                toggleMenu();
-                                burger.current.classList.toggle(classes.isActive);
-                            }}>Services</Link>
-                            <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
-                                toggleMenu();
-                                burger.current.classList.toggle(classes.isActive);
-                            }}>Projets</Link>
-                            <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
-                                toggleMenu();
-                                burger.current.classList.toggle(classes.isActive);
-                            }}>A propos</Link>
-                            <div className={classes.buttonWrapper}>
+                    <motion.div className={`${classes.mobileNav} `}
+                        initial="closed" animate={isMenuOpen ? "open" : "closed"} variants={sideBar} >
+                        {/* ${isMenuOpen ? classes.active : ""} */}
+                        <motion.div className={classes.mobileLinks}
+                            initial="closed"
+                            animate={isMenuOpen ? "open" : "closed"}
+                            variants={sideVariants}>
+                            <motion.div variants={variantsItem}>
+                                <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
+                                    toggleMenu();
+                                    burger.current.classList.toggle(classes.isActive);
+                                }}>Accueil</Link>
+                            </motion.div>
+                            <motion.div variants={variantsItem}>
+                                <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
+                                    toggleMenu();
+                                    burger.current.classList.toggle(classes.isActive);
+                                }}>Services</Link>
+                            </motion.div>
+                            <motion.div variants={variantsItem}>
+                                <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
+                                    toggleMenu();
+                                    burger.current.classList.toggle(classes.isActive);
+                                }}>Projets</Link>
+                            </motion.div>
+                            <motion.div variants={variantsItem}>
+                                <Link href="/#projects-section" className={classes.mobileLink} onClick={() => {
+                                    toggleMenu();
+                                    burger.current.classList.toggle(classes.isActive);
+                                }}>A propos</Link>
+                            </motion.div>
+                            <motion.div className={classes.buttonWrapper} variants={variantsItem}>
 
                                 <Link href="/book-a-call" className='primary-button'>Me contacter</Link>
-                            </div>
-                        </div>
-                        <div className={classes.socials}>
-                            <Image src={tiktokLogo} alt='logo tiktok' className={classes.socialLogo} />
-                            <Image src={instaLogo} alt='logo instagram' className={classes.socialLogo} />
-                            <Image src={linkedinLogo} alt='logo linkedin' className={classes.socialLogo} />
-                        </div>
-                    </div>
+                            </motion.div>
+                        </motion.div>
+                        <motion.div className={classes.socials} variants={sideVariantsSocials}>
+                            <motion.div variants={variantsItem}>
+                                <Image src={tiktokLogo} alt='logo tiktok' className={classes.socialLogo} />
+                            </motion.div>
+                            <motion.div variants={variantsItem}>
+
+                                <Image src={instaLogo} alt='logo instagram' className={classes.socialLogo} />
+                            </motion.div>
+                            <motion.div variants={variantsItem}>
+                                <Image src={linkedinLogo} alt='logo linkedin' className={classes.socialLogo} />
+                            </motion.div>
+                        </motion.div>
+                    </motion.div>
                 </div>
             </nav>
         </header>
