0250eea8d7c4818ccdc444282b267658e103b8ae
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-06-26 11:24:44 +0200

    Désactiver reactStrictMode pour la prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: !isProd,
 }
 
 module.exports = nextConfig
