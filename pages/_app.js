d1cd46a869cdb56d7a15a54d7d80badbfa2db015
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-07-01 10:10:30 +0200

    Retirer balise ga4 pour l'intégrer via gtm

--- pages/_app.js
+++ pages/_app.js
@@ -25,14 +25,7 @@ export default function App({ Component, pageProps }) {
       src='https://www.googletagmanager.com/gtag/js?id=G-S70VDK1DHC'
       strategy='afterInteractive'
     />
-    <Script id="google-analytics" strategy='afterInteractive'>
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-S70VDK1DHC');
-      `}
-    </Script>
+
 
   </GlobalContextProvider>
 }
